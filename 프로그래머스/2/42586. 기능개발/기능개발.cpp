44b2c5fc83cf61471d67e29a825c58e78df260d1
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-04-17 13:26:17 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -3,66 +3,37 @@
 #include <stack>
 using namespace std;
 
-struct process{
-    int progress;
-    int speed;
-};
 
-//각 작업속도 맞게, 구조체를 만든다. 
-//그 구조체대로, while(100인애가 없을때까지 더한다.)
-
-//스택에 넣는 건, index를 옮겨가며,앞 순서가 100이상이 될 시 뒷순서가 100이상인 동안 계속 스택에 넣는다. 
-//그리고 정답에 스택크기를 넣고, 스택을 모두 팝한다.
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    int n=progresses.size();
-    vector<process> processArr(n+1);
     
-    for(int i=1;i<=n;i++){
-        processArr[i].progress=progresses[i-1];
-        processArr[i].speed=speeds[i-1];
+    int day;//이번 작업이 끝나는 데 걸리는 날짜
+    int max_day=0;//현재 배포 그룹에서 가장 오래 걸리는 대장의 날짜
+    
+    for(int i=0;i<progresses.size();i++){
+        //완료 날짜 계산
+        day=(99-progresses[i])/speeds[i]+1;
+        
+        //배포 그룹 묶기
+        //아예 처음이거나, 이번 작업이 이전 대장보다 오래 걸린다면?
+        if(answer.empty()||max_day<day){
+            //앞기차는 보내고 내가 새로운 대장이 되어 새 배포그룹을 만듦.
+            answer.push_back(1);
+        }
+        else{
+            //지금 작업이 대장보다 일찍 끝난다면
+            //대장 나갈 때 같이 나감
+            ++answer.back();
+        }
+        
+        //대장 정보 갱신
+        //만약 이번 작업이 지금까지의 대장보다 늦게 끝난다면
+        //이제부터 얘가 대장임
+        if(max_day<day){
+            max_day=day;
+        }
     }
-    
-    int index=1;
-    
-    while(true){
-        //TODO: end condition: 모두 100이상이면 break;
-        bool endCondition=true;
-        for(int i=1;i<=n;i++){
-            if(processArr[i].progress<100){
-                endCondition=false;
-            }
-        }
-        
-        if(endCondition){
-            break;
-        }
-        
-        //1초마다(WHILE문마다) 작업량만큼 더하기
-        for(int i=1;i<=n;i++){
-            processArr[i].progress+=processArr[i].speed;
-        }
-        
-        stack<int> succeedIndexStack;
-        
-        //표준인덱스 기준으로 100이상이면
-        while(index<=n&&processArr[index].progress>=100)
-        {
-            //작업완료에 넣고
-            succeedIndexStack.push(index);
-            //다음것도 확인 위해 인덱스 이동
-            index++;
-        }
-        
-        
-        //작업완료된 게 있으면
-        if(succeedIndexStack.size()!=0){
-            //ANSWER에 작업완료된 양만큼 넣기
-            answer.push_back(succeedIndexStack.size());    
-        }
-    }    
-    
     
     return answer;
 }
